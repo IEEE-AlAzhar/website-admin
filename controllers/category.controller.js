7cc47646086fc4e38006b9cb4d89004645de22c2
mohamedsaad <mahameds19999@gmail.com>
2020-05-12 23:58:12 +0200

    Edits

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -5,7 +5,7 @@ const verifyToken = require("../helpers/verifyToken");
 const CategoryService = require("../services/category/category.service");
 let categoryService = new CategoryService();
 
-server.get("/list", verifyToken(), categoryService.listRecords);
+server.get("/list", categoryService.listRecords);
 server.post("/new", verifyToken(), categoryService.createRecord);
 server.put("/:id", verifyToken(), categoryService.updateRecord);
 server.delete("/:id", verifyToken(), categoryService.deleteRecord);
